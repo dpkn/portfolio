170409de12f9b92508ff2a3043a498c4d3c4a5b6
daniel <9087597+daankrs@users.noreply.github.com>
2018-09-06 16:34:54 +0200

    Put navigation in a component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <MainNavigation></MainNavigation>
     <router-view/>
   </div>
 </template>
 
+<script>
+import MainNavigation from '@/components/MainNavigation.vue';
+
+export default {
+  name: 'app',
+  components: {
+    MainNavigation,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar">
+    <h1>DANIEL KORSSEN</h1>
+    <router-link to="/">Homeee</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MainNavigation',
+};
+</script>
+
+<style scoped lang="scss">
+.navbar{
+  width:100%;
+  background: #fff;
+  color:#000;
+}
+</style>
