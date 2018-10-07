8a8791cf1d8fa366090ea00f17e33adcb7643489
daniel <9087597+daankrs@users.noreply.github.com>
2018-10-07 16:13:53 +0200

    Made components static

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
+    <MainNavigation></MainNavigation>
     <transition name="fade" mode="out-in">
       <router-view></router-view>
+    </transition>
+    <MainFooter></MainFooter>
+
+    <transition name="fade" mode="out-in">
+      <router-view name="modal"></router-view>
     </transition>
   </div>
 </template>
 
 <script>
+import MainNavigation from '@/components/MainNavigation.vue';
+import MainFooter from '@/components/MainFooter.vue';
+
 export default {
   name: 'app',
+  components: {
+    MainNavigation,
+    MainFooter,
+  },
   mounted() {
     this.switchStyle(this.$route.query.style);
   },
