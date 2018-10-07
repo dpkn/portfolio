f640ff86527302d9d3e308214056d2db1afed87e
daniel <9087597+daankrs@users.noreply.github.com>
2018-10-07 15:23:50 +0200

    Created styleswitch functionality

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,26 @@
 <script>
 export default {
   name: 'app',
+  mounted() {
+    this.switchStyle(this.$route.query.style);
+  },
+  watch: {
+    /* eslint func-names: ["error", "never"] */
+    // This function is called whenever the ?style parameter in the url
+    // changes. It switches between two website themes by applying a class to the #app
+    // element
+    '$route.query.style': function (style) {
+      this.switchStyle(style);
+    },
+  },
+  methods: {
+    switchStyle(style) {
+      if (style === '2') {
+        document.getElementById('app').className = 'alternative';
+      } else {
+        document.getElementById('app').className = '';
+      }
+    },
+  },
 };
 </script>
