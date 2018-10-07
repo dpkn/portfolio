dbeb2efb1b5c2cc982d3892b224193e57c3af9e4
daniel <9087597+daankrs@users.noreply.github.com>
2018-10-07 15:24:32 +0200

    Fixed form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="test">
+  <div id="contact">
     <MainNavigation></MainNavigation>
-    <h1>contact</h1>
-    <form action="">
+    <div class="container">
+      <form method="POST" action="http://home.hku.nl/cgi-bin/FormMail.cgi">
+        <input type="hidden" name="recipient" value="daan.korssen@student.hku.nl">
+        <input type="hidden" name="subject" value="Feedback van website via formulier">
+        <input type="hidden" name="redirect" value="http://studenthome.hku.nl/~iris.schuit/contact/send">
+        <input type="hidden" name="required" value="email,realname">
 
-    </form>
+        Email: <br> <input type="text" name="email" size="32" maxlength="40"><br>
+        Naam: <br><input type="text" name="realname" size="32" maxlength="32"><br>
+        Opmerkingen: <br><textarea name="opm" rows="3" cols="40">Verdere opmerkingen: </textarea><br>
+        <input type="submit" value="Verstuur mijn aanvraag">
+      </form>
+    </div>
   </div>
 </template>
 
