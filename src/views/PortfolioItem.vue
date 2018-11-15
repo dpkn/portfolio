7fec3c331f3dd5030b93a4347ebb73aba81730b5
daniel <9087597+daankrs@users.noreply.github.com>
2018-11-15 20:15:02 +0100

    Cleaner way of presenting html template

--- src/views/PortfolioItem.vue
+++ src/views/PortfolioItem.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="portfolio-item">
+  <div class="portfolio-item-overview">
     <div class="info container">
-      <router-link to="/" class="back-button">&larr; PORTFOLIO</router-link>
-      <h1>{{item.title}}</h1>
-      <h2 v-if="item.subtitle">
-        {{item.subtitle}}
-      </h2>
-      <p v-if="item.description">
-        {{item.description}}
-      </p>
-      <p v-if="item.cta" v-html="item.cta">
-      </p>
+      <router-link :to="{name:'portfolio'}" class="back-button">&larr; PORTFOLIO</router-link>
+
+      <h1 v-if="item.title" v-html="item.title"></h1>
+      <h2 v-if="item.subtitle" v-html="item.subtitle"></h2>
+      <p v-if="item.description" v-html="item.description"></p>
+      <p class="cta" v-if="item.cta" v-html="item.cta"></p>
     </div>
-    <div :class="item.layout">
-      <img :src="contents.url" v-for="(contents,key) in item.content"
-      :key="key" class="item" :class="contents.class">
-    </div>
+    <transition name="fade" mode="out-in">
+      <div id="portfolio-content" :class="item.layout" v-show="imagesLoaded">
+        <img :src="contents.url" v-for="(contents,key) in item.content"
+        :key="key" class="item" :class="contents.class">
+      </div>
+  </transition>
   </div>
 </template>
 
@@ -24,12 +22,11 @@
 import imagesLoaded from 'imagesloaded';
 import axios from 'axios';
 
-
 export default {
   name: 'PortfolioItem',
   data() {
     return {
-      items: {},
+      imagesLoaded: false,
       item: {
         title: '',
         content: '',
@@ -39,40 +36,46 @@ export default {
   },
   methods: {
     getItemContent(id) {
-      axios.get('portfolio.json')
-        .then((response) => {
-          console.log(response, id);
-          // handle success
-          this.items = response.data;
-          if (this.items[id]) {
-            this.item = this.items[id];
-          } else {
-            // If the provided id doesn't match with a portfolio entry, route back to home
-            this.$router.push('/');
-          }
-        });
+      axios.get('portfolio.json').then((response) => {
+        if (response.data[id]) {
+          this.item = response.data[id];
+        } else {
+          // If the provided id doesn't match with a portfolio entry, route back to home
+          console.log('cant fin');
+        }
+      });
     },
   },
   mounted() {
-    // FIXME: : Figure out why loading callback does not work ksfdskjklddksaldjksakljdaklj
     this.getItemContent(this.$route.params.id);
-
-    const imgLoad = imagesLoaded('.filmstrip');
-    imgLoad.on('progress', (instance, image) => {
-      console.log(instance, image);
-    });
+  },
+  updated() {
+    const imgLoad = imagesLoaded('#portfolio-content');
     imgLoad.on('done', (instance, image) => {
+      this.imagesLoaded = true;
       console.log(instance, image);
     });
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.portfolio-item-overview{
+  background: #fff;
   width: 100%;
+  height: 100%;
+  position: fixed;
+  top:0;
+  left:0;
+  z-index: 9999;
+  overflow-y: auto;
+}
 .info{
-  margin:70px auto;
-  text-align: left;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+.cta a{
+  font-weight: 700;
 }
 .back-button{
   color:#000;
@@ -94,14 +97,27 @@ p a{
   .item{
     display: inline-block;
     width: 50%;
-    margin-top: -4px;
+    float:left;
+    &.hundred{
+      width: 100%;
+    }
+    &.eighty{
+      width: 80%;
+    }
     &.sixty{
       width: 60%;
     }
     &.fourty{
       width: 40%;
     }
+    &.twenty{
+      width: 20%;
+    }
+    @media only screen and (max-width: 1000px){
+      width: 100%!important;
+    }
   }
+  @include clearfix;
 }
 
 .filmstrip {
@@ -122,5 +138,6 @@ p a{
     width: 400px;
     background: #f2f2f2;
   }
+  padding-bottom: 50px;
 }
 </style>
