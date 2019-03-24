a51e5121b1e34f2c8c6f1c5cfc03af5728b9395a
daniel <9087597+daankrs@users.noreply.github.com>
2019-03-24 11:45:25 +0100

    New lead text

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -1,14 +1,19 @@
 <template>
   <section class="hero container">
     <img class="headshot pull-right" src="/headshot.jpg" alt="Headshot" />
-    <h1 class="pull-left">Hi! I am Dani&euml;l, a <span>creative developer</span>
-      working and experimenting at the intersection of art and technology.
-      I'm currently studying <span>Interaction Design</span> at <a href="https://www.hku.nl/en.htm" target="_blank">HKU</a>.
-      You can find me on
-      <a href="https://github.com/dpkn" target="_blank">GitHub</a>,
-      <a href="http://linkedin.com/in/danielkorssen/" target="_blank">LinkedIn</a>,
-      or <a href="#" onclick="this.href=atob('bWFpbHRvOmRhYW5rb3Jzc2VuJTQwZ21haWwuY29t'); return true">write me a message.</a>
-    </h1>
+    <div class="pull-left">
+      <h1>
+        I'm a <span>creative developer</span>
+        working and experimenting at the intersection of art and technology, with a focus on web technologies.
+        I'm currently studying <span>Interaction Design</span> at <a href="https://www.hku.nl/en.htm" target="_blank">HKU</a>.
+      </h1>
+      <p>
+        <a href="https://github.com/dpkn" target="_blank">GitHub</a>,
+        <a href="https://twitter.com/danielkorssen" target="_blank">Twitter</a>,
+        <a href="http://linkedin.com/in/danielkorssen/" target="_blank">LinkedIn</a>,
+        <a href="#" onclick="this.href=atob('bWFpbHRvOmRhYW5rb3Jzc2VuJTQwZ21haWwuY29t'); return true">email</a>
+      </p>
+    </div>
   </section>
 </template>
 
@@ -23,13 +28,16 @@ export default {
   padding:40px 0 80px 0;
   @include clearfix;
 }
-.hero h1{
+.pull-left{
+  max-width: 700px;
+}
+h1{
   font-size: 1.7em;
   font-weight: 500;
+  font-family: 'Esteban', serif;
   & span{
     color:#F39237;
   }
-  max-width: 700px;
 }
 
 .headshot{
