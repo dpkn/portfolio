ac000ed50bf0a19182ecdc41a910880e81c4fc1d
daniel <9087597+daankrs@users.noreply.github.com>
2019-10-11 12:48:53 +0200

    Added keep-alive so portfolio content does not to be reloaded

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <MainNavigation></MainNavigation>
     <transition name="fade" mode="out-in">
-      <router-view></router-view>
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
     </transition>
     <MainFooter></MainFooter>
 
